d3faaa3012a0459d2bf5833aa85a4f4849509224
Joel Weiser <joel.weiser@oicr.on.ca>
2018-03-19 15:11:17 -0400

    beginning of new QA check

---
+++ ContributorsCheck/contributors-check/src/main/java/org/reactome/contributors_check/ContributorsCheck.java
@@ -0,0 +1,13 @@
+package org.reactome.contributors_check;
+
+/**
+ * Hello world!
+ *
+ */
+public class ContributorsCheck 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
