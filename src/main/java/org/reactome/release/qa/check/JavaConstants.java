58e9eafdec55b5f9153bc5feb6c3646cee5e86aa
FredLoney <loneyf@ohsu.edu>
2018-07-26 18:26:41 -0700

    Add templateEvent and entityOnOtherCell.

--- src/main/java/org/reactome/release/qa/check/JavaConstants.java
+++ src/main/java/org/reactome/release/qa/check/JavaConstants.java
@@ -5,5 +5,7 @@ public class JavaConstants {
     // TODO - move these to ReactomeJavaConstants.
 
     public static final String Publication = "Publication";
+    public static final String templateEvent = "templateEvent";
+    public static final String entityOnOtherCell = "entityOnOtherCell";
 
 }
