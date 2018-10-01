11b26839675329b3dc9ad9e64bafcc7e0faa9c57
FredLoney <loneyf@ohsu.edu>
2018-10-01 14:51:29 -0700

    Add a run-time retention annotation to avoid Eclipse and Maven problems.

--- src/main/java/org/reactome/release/qa/package-info.java
+++ src/main/java/org/reactome/release/qa/package-info.java
@@ -1,8 +1,10 @@
 /**
- * 
- */
-/**
  * @author wug
  *
  */
+// Must add a run-time retention annotation to avoid an
+// Eclipse error. This can also cause a problem with Maven
+// (cf. https://stackoverflow.com/questions/6770455/maven-compiling-package-info-java-to-package-info-class).
+// The work-around is to add an extraneous Deprecated annotation.
+@Deprecated
 package org.reactome.release.qa;
