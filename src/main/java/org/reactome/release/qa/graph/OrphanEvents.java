4c39971b7bcebabf977cbbc58633f0c9debf09c9
FredLoney <loneyf@ohsu.edu>
2018-10-22 18:15:14 -0700

    Don't report chimeric events.

--- src/main/java/org/reactome/release/qa/graph/OrphanEvents.java
+++ src/main/java/org/reactome/release/qa/graph/OrphanEvents.java
@@ -41,6 +41,9 @@ public class OrphanEvents extends AbstractQACheck {
         dba.loadInstanceReverseAttributeValues(events, loadAtts);
         // Check for events which are not referenced by another event.
         for (GKInstance event: events) {
+            // A chimeric is used for inference, which doesn't need to be in the hierachy.
+            if (QACheckerHelper.isChimeric(event))
+                continue;
             Collection<GKInstance> referers = event.getReferers(ReactomeJavaConstants.hasEvent);
             if ((referers == null || referers.isEmpty()) && !tlps.contains(event)) {
                 addReportLine(report, event);
