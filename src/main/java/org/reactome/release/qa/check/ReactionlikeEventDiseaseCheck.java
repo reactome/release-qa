0e754e3902e816bf6f205be52bf41fd330bb6ced
FredLoney <loneyf@ohsu.edu>
2018-10-31 17:36:27 -0700

    Move the MalformedReactionlikeEventCheck RLE disease check into ReactionlikeEventDiseaseCheck.

---
+++ src/main/java/org/reactome/release/qa/check/ReactionlikeEventDiseaseCheck.java
@@ -0,0 +1,62 @@
+package org.reactome.release.qa.check;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.gk.model.GKInstance;
+import org.gk.model.ReactomeJavaConstants;
+import org.reactome.release.qa.annotations.SliceQACheck;
+import org.reactome.release.qa.common.AbstractQACheck;
+import org.reactome.release.qa.common.QACheckerHelper;
+import org.reactome.release.qa.common.QAReport;
+
+/**
+ * Reports non-failed RLEs with a normalReaction value but without a disease value.
+ */
+@SliceQACheck
+public class ReactionlikeEventDiseaseCheck extends AbstractQACheck {
+	
+    private static final String[] LOAD_ATTS = { ReactomeJavaConstants.disease };
+    
+	public ReactionlikeEventDiseaseCheck() {
+	}
+	
+	@Override
+    public String getDisplayName() {
+        return "ReactionlikeEvent_Not_Failed_With_Normal_Without_Disease";
+    }
+
+    @Override
+	public QAReport executeQACheck() throws Exception {
+	    QAReport report = new QAReport();
+        // The RLEs which refer to a normal reaction.
+	    Collection<GKInstance> normalReferers =
+	            QACheckerHelper.getInstancesWithNonNullAttribute(dba,
+	                    ReactomeJavaConstants.ReactionlikeEvent,
+	                    ReactomeJavaConstants.normalReaction, null);
+	    // Prep the RLEs for accessing the disease attribute.
+	    dba.loadInstanceAttributeValues(normalReferers, LOAD_ATTS);
+	    for (GKInstance rle: normalReferers) {
+	        if (isEscaped(rle)) {
+	            continue;
+	        }
+	        // Ignore failed reactions.
+            if (!rle.getSchemClass().isa(ReactomeJavaConstants.FailedReaction)) {
+                // Report non-disease RLEs.
+                if (rle.getAttributeValue(ReactomeJavaConstants.disease) == null) {
+                    List<String> line = Arrays.asList(
+                            rle.getDBID().toString(),
+                            rle.getDisplayName(),
+                            rle.getSchemClass().getName(),
+                            QACheckerHelper.getLastModificationAuthor(rle));
+                    report.addLine(line);
+                }
+	        }
+	    }
+	    report.setColumnHeaders(Arrays.asList("DB_ID","DisplayName", "Class", "MostRecentAuthor"));
+	    
+	    return report;
+	}
+
+}
