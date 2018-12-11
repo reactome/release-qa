b08b3c7af2b9cfd147defbdcacaefdadf753e5ca
FredLoney <loneyf@ohsu.edu>
2018-12-11 13:03:13 -0800

    Remove unused import.

--- src/main/java/org/reactome/release/qa/diagram/DiagramOverlapCheck.java
+++ src/main/java/org/reactome/release/qa/diagram/DiagramOverlapCheck.java
@@ -2,7 +2,6 @@ package org.reactome.release.qa.diagram;
 
 import java.awt.Rectangle;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Predicate;
