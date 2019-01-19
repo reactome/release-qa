96afb6bae6759335e7289c7a36a6b37353854813
FredLoney <loneyf@ohsu.edu>
2019-01-18 16:45:36 -0800

    Work around a schema corruption bug due to the Maven jar builder.

--- src/main/java/org/reactome/release/qa/graph/ReactionsSingleInputOutputSchemaClassMismatchCheck.java
+++ src/main/java/org/reactome/release/qa/graph/ReactionsSingleInputOutputSchemaClassMismatchCheck.java
@@ -3,7 +3,9 @@ package org.reactome.release.qa.graph;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
+import org.apache.log4j.Logger;
 import org.gk.model.GKInstance;
 import org.gk.model.ReactomeJavaConstants;
 import org.gk.schema.SchemaClass;
@@ -14,6 +16,9 @@ import org.reactome.release.qa.common.QAReport;
 
 @GraphQACheck
 public class ReactionsSingleInputOutputSchemaClassMismatchCheck extends AbstractQACheck {
+
+    private static final Logger logger =
+            Logger.getLogger(ReactionsSingleInputOutputSchemaClassMismatchCheck.class);
 
     private static final List<String> HEADERS = Arrays.asList(
             "DBID", "DisplayName", "SchemaClass", "MostRecentAuthor");
@@ -43,7 +48,6 @@ public class ReactionsSingleInputOutputSchemaClassMismatchCheck extends Abstract
                 cls.isa(ReactomeJavaConstants.Depolymerisation) ||
                 cls.isa(ReactomeJavaConstants.BlackBoxEvent))
                 continue;
-            // Check for incompatible input/output singletons.
             List<GKInstance> inputs =
                     rle.getAttributeValuesList(ReactomeJavaConstants.input);
             if (inputs.size() == 1) {
@@ -52,7 +56,32 @@ public class ReactionsSingleInputOutputSchemaClassMismatchCheck extends Abstract
                 if (outputs.size() == 1) {
                     GKInstance input = inputs.get(0);
                     GKInstance output = outputs.get(0);
-                    if (input.getSchemClass() != output.getSchemClass()) {
+                    SchemaClass inputSchemaCls = input.getSchemClass();
+                    SchemaClass outputSchemaCls = output.getSchemClass();
+                    if (inputSchemaCls != outputSchemaCls) {
+                        // Work around a possible schema corruption bug.
+                        // The Maven single jar package assembler adds
+                        // two GKSchemaClass.class files, which can sporadically
+                        // result in two SchemaClass instances with the same name.
+                        //
+                        // This problem was first noticed for the 2018-12-17
+                        // weekly QA check run. At that time, the problem occured
+                        // when this QA check is run along with other QA checks, but
+                        // not when it is run alone. Oddly, the problem did not occur
+                        // when the continue; line below was commented out. All of which
+                        // indicates that the problem is sporadic and unpredictable.
+                        //
+                        // The work-around is to log the error and continue.
+                        // Oddly, only one or two issues were reported without the
+                        // logging but about 20 error messages were logged.
+                        //
+                        // TODO - consider using the maven shading package, although
+                        // that is a complex, ugly solution.
+                        if (Objects.equals(inputSchemaCls.getName(), outputSchemaCls.getName())) {
+                            logger.error("Two schema class instances have the same name: " +
+                                    inputSchemaCls + " and " + outputSchemaCls);
+                            continue;
+                        }
                         addReportLine(report, rle);
                     }
                 }
