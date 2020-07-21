78472cac719ffa704e504da7beef2f4d42222920
Justin Cook <cookersjs@gmail.com>
2020-07-21 15:22:40 -0400

    Move all new (Justin) QA checks to branch off master

--- src/main/java/org/reactome/release/qa/common/QACheckerHelper.java
+++ src/main/java/org/reactome/release/qa/common/QACheckerHelper.java
@@ -3,12 +3,7 @@ package org.reactome.release.qa.common;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import org.gk.model.GKInstance;
@@ -290,6 +285,228 @@ public class QACheckerHelper {
             return originCls.getName() + "_2_" + attribute.getName();
         else
             return originCls.getName();
+    }
+
+    /**
+     * Finds all Events in DB that are not used for manual inference, or members of Pathways in skiplist. Finding if
+     * manually inferred is done by checking for a null 'inferredFrom' referral.
+     * @param dba MySQLAdaptor
+     * @param skiplistDbIds List<String> -- List of Pathway DbIds. If an Event being checked is a member of these pathways, they are skipped.
+     * @return Set<GKInstance> -- All Events in DB that are not used for manual inference.
+     * @throws Exception -- Thrown by MySQLAdaptor
+     */
+    public static Set<GKInstance> findEventsNotUsedForManualInference(MySQLAdaptor dba, List<String> skiplistDbIds) throws Exception {
+        Set<GKInstance> eventsNotUsedForInference = new HashSet<>();
+        Collection<GKInstance> events = dba.fetchInstancesByClass(ReactomeJavaConstants.Event);
+        for (GKInstance event : events) {
+            if (!manuallyInferred(event) && !memberSkipListPathway(event, skiplistDbIds)) {
+                eventsNotUsedForInference.add(event);
+            }
+        }
+        return eventsNotUsedForInference;
+    }
+
+    /**
+     * Checks if incoming Event is manually inferred by checking inferredFrom referral.
+     * This method only correctly identifies manual inferences in a curation or slice database.
+     * This is due to the fact that it only checks the 'inferredFrom' attribute, which is used
+     * in automatic inferences as well.
+     * @param event GKInstance -- Event instance being checked for inferredFrom referral.
+     * @return boolean -- true if inferredFrom referral exists, false if not.
+     * @throws Exception -- Thrown by MySQLAdaptor.
+     */
+    private static boolean manuallyInferred(GKInstance event) throws Exception {
+        return event.getReferers(ReactomeJavaConstants.inferredFrom) != null;
+    }
+
+    /**
+     * Finds all Events not used for Inference, and then finds subset that are Human ReactionlikeEvents
+     * @param dba MySQLAdaptor
+     * @param skiplistDbIds List<String> -- List of Pathway DbIds. If an Event being checked is a member of these pathways, they are skipped.
+     * @return Set<GKInstance> -- All Human ReactionlikeEvents that are not used for manual inference.
+     * @throws Exception-- Thrown by MySQLAdaptor
+     */
+    public static Set<GKInstance> findHumanReactionsNotUsedForManualInference(MySQLAdaptor dba, List<String> skiplistDbIds) throws Exception {
+        Set<GKInstance> reactionsNotUsedForManualInference = new HashSet<>();
+        for (GKInstance event : findEventsNotUsedForManualInference(dba, skiplistDbIds)) {
+            // Filter for Human ReactionlikeEvents
+            if (event.getSchemClass().isa(ReactomeJavaConstants.ReactionlikeEvent)
+                    && isHumanDatabaseObject(event)) {
+
+                reactionsNotUsedForManualInference.add(event);
+            }
+        }
+        return reactionsNotUsedForManualInference;
+    }
+
+    /**
+     * This method utilizes the InstanceUtilities method for finding all reaction participants. It also checks for the
+     * 'activeUnit' participant in catalysts/regulations that can be flagged by certain QA checks. Generally speaking,
+     * the 'activeUnit' instance should be found in the 'physicalEntity' of catalysts/regulations. Sometimes this is not
+     * the case, when the wrong activeUnit is mistakenly checked in. This will be flagged by QA tests that call this method.
+     * @param reaction GKInstance -- ReactionlikeEvent that will be checked for all participants.
+     * @return Set<GKInstance> -- All distinct participants in the incoming ReactionlikeEvent.
+     * @throws Exception -- Thrown by MySQLAdaptor.
+     */
+    public static Set<GKInstance> getAllReactionParticipantsIncludingActiveUnits(GKInstance reaction) throws Exception {
+        Set<GKInstance> reactionPEs = new HashSet<>();
+        reactionPEs.addAll(InstanceUtilities.getReactionParticipants(reaction));
+        // Retrieve activeUnit PEs from Reaction Catalysts/Regulations, if present.
+        reactionPEs.addAll(getContainedActiveUnits(reaction));
+
+        Set<GKInstance> allReactionPEs = new HashSet<>();
+        for (GKInstance reactionPE : reactionPEs) {
+            allReactionPEs.add(reactionPE);
+            allReactionPEs.addAll(getPhysicalEntityContainedInstances(reactionPE));
+        }
+        return allReactionPEs;
+    }
+
+    /**
+     * This method returns all instances found in the 'activeUnit' slot of the incoming Reaction's 'catalystActivity'
+     * and 'regulatedBy' instances, if they exist.
+     * @param reaction GKInstance -- ReactionlikeEvent that will be checked for all activeUnit instances.
+     * @return Set<GKInstance> -- All distinct activeUnit participants in the incoming ReactionlikeEvent.
+     * @throws Exception -- Thrown by MySQLAdaptor.
+     */
+    private static Set<GKInstance> getContainedActiveUnits(GKInstance reaction) throws Exception {
+        Set<GKInstance> reactionCatalystsAndRegulations = new HashSet<>();
+        reactionCatalystsAndRegulations.addAll(reaction.getAttributeValuesList(ReactomeJavaConstants.catalystActivity));
+        reactionCatalystsAndRegulations.addAll(reaction.getAttributeValuesList(ReactomeJavaConstants.regulatedBy));
+
+        Set<GKInstance> activeUnits = new HashSet<>();
+        for (GKInstance instance : reactionCatalystsAndRegulations) {
+            activeUnits.addAll(InstanceUtilities.getContainedInstances(
+                    instance,
+                    ReactomeJavaConstants.activeUnit
+            ));
+        }
+        return activeUnits;
+    }
+
+    /**
+     * This method is wrapper for the InstanceUtilities getContainedInstances method.
+     * @param reactionPE GKInstance -- PhysicalEntity that will be checked for contained instances.
+     * @return Set<GKInstance> -- All distinct contained instances in the incoming PhysicalEntity.
+     * @throws Exception -- Thrown by MySQLAdaptor.
+     */
+    public static Set<GKInstance> getPhysicalEntityContainedInstances(GKInstance reactionPE) throws Exception {
+        return InstanceUtilities.getContainedInstances(
+                reactionPE,
+                ReactomeJavaConstants.hasMember,
+                ReactomeJavaConstants.hasCandidate,
+                ReactomeJavaConstants.hasComponent,
+                ReactomeJavaConstants.repeatedUnit
+        );
+    }
+
+    /**
+     * Checks if incoming DatabaseObject (Event or PhysicalEntity) has single Homo sapiens species.
+     * @param databaseObject GKInstance -- Event or PhysicalEntity instance being checked for only Homo sapiens species.
+     * @return boolean -- true if databaseObject only has a single, Homo sapiens species instance, false if not.
+     * @throws Exception -- Thrown by MySQLAdaptor.
+     */
+    public static boolean isHumanDatabaseObject(GKInstance databaseObject) throws Exception {
+
+        List<GKInstance> objectSpecies = databaseObject.getSchemClass().isValidAttribute(ReactomeJavaConstants.species) ?
+                databaseObject.getAttributeValuesList(ReactomeJavaConstants.species) : Collections.emptyList();
+        return objectSpecies.size() == 1 && hasHumanSpecies(objectSpecies);
+    }
+
+    /**
+     * Checks if the incoming DatabaseObject (Event or PhysicalEntity) is non-human.
+     * @param databaseObject GKInstance -- Event or PhysicalEntity to be checked for non-human species attribute.
+     * @return boolean -- true if instance has species, and if none of the species are human, false if not.
+     * @throws Exception -- Thrown by MySQLAdaptor.
+     */
+    public static boolean hasOnlyNonHumanSpecies(GKInstance databaseObject) throws Exception {
+        // Check if species is a valid attribute for physicalEntity.
+        return hasSpeciesAttribute(databaseObject)
+                && databaseObject.getAttributeValue(ReactomeJavaConstants.species) != null
+                && !databaseObject.getAttributeValuesList(ReactomeJavaConstants.species).isEmpty()
+                && !hasHumanSpecies(databaseObject.getAttributeValuesList(ReactomeJavaConstants.species));
+    }
+
+    /**
+     * Checks to see if any of the Species instances in incoming list are Homo sapiens.
+     * @param objectSpecies List<GKInstance> -- List of Species instances from a DatabaseObject (PhysicalEntity or ReactionlikeEvent)
+     * @return boolean -- true if any of the Species instance display names are equal to Homo sapiens.
+     */
+    private static boolean hasHumanSpecies(List<GKInstance> objectSpecies) {
+        return objectSpecies.stream().anyMatch(species -> species.getDisplayName().equals("Homo sapiens"));
+    }
+
+    /**
+     * Checks if species is a valid attribute in the incoming PhysicalEntity.
+     * @param physicalEntity GKInstance -- PhysicalEntity that is being checked to see if species is a valid attribute.
+     * @return boolean -- true if species is a valid attribute, false if not.
+     */
+    private static boolean hasSpeciesAttribute(GKInstance physicalEntity) {
+        return physicalEntity.getSchemClass().isValidAttribute(ReactomeJavaConstants.species);
+    }
+
+    /**
+     * Checks if the incoming databaseObject has a popalated disease attribute.
+     * @param databaseObject GKInstance -- Instance to be checked for populated disease attribute.
+     * @return boolean -- true if has filled disease attribute, false if not.
+     * @throws Exception -- Thrown by MySQLAdaptor.
+     */
+    public static boolean hasDisease(GKInstance databaseObject) throws Exception {
+        return databaseObject.getSchemClass().isValidAttribute(ReactomeJavaConstants.disease)
+                && databaseObject.getAttributeValue(ReactomeJavaConstants.disease) != null;
+    }
+
+    /**
+     * Finds all parent DbIds of the incoming Event, and then checks if any of them are in the skiplist of Pathway DbIds.
+     * @param event GKInstance -- Event that is being checked for membership in a skiplist Pathway.
+     * @param skiplistDbIds List<String> -- List of Pathway DbIds. If an Event being checked is a member of these pathways, they are skipped.
+     * @return boolean -- true if member of skiplist Pathway, false if not.
+     * @throws Exception -- Thrown by MySQLAdaptor.
+     */
+    public static boolean memberSkipListPathway(GKInstance event, List<String> skiplistDbIds) throws Exception {
+        // Finds all parent Event DbIds.
+        Set<String> hierarchyDbIds = findEventHierarchyDbIds(event);
+        // Check if any returned Event DbIds (including original Events) are in skiplist.
+        return skiplistDbIds.stream().anyMatch(dbId -> hierarchyDbIds.contains(dbId));
+    }
+
+    /**
+     * Finds parent DbIds of incoming Event through hasEvent referrers. Recurses until there are no more referrers.
+     * @param event GKInstance -- Event that is being checked for referrers. Its DbId is added to the Set being built.
+     * @return Set<String> -- Once TopLevelPathway has been found, returns all DbIds, inclusive, between TopLevelPathway and original Event.
+     * @throws Exception -- Thrown by MySQLAdaptor.
+     */
+    private static Set<String> findEventHierarchyDbIds(GKInstance event) throws Exception {
+        Set<String> dbIds = new HashSet<>();
+        dbIds.add(event.getDBID().toString());
+        Collection<GKInstance> hasEventReferrals = event.getReferers(ReactomeJavaConstants.hasEvent);
+        if (hasEventReferrals != null) {
+            for (GKInstance hasEventReferral : hasEventReferrals) {
+                dbIds.addAll(findEventHierarchyDbIds(hasEventReferral));
+            }
+        }
+        return dbIds;
+    }
+
+    /**
+     * Reads skiplist file that contains Pathway DbIds that should not be included in QA check.
+     * @return List<String> -- List of DbIds.
+     */
+    public static List<String> getNonHumanPathwaySkipList() throws IOException {
+        return Files.readAllLines(Paths.get("resources/manually_curated_nonhuman_pathways_skip_list.txt"));
+    }
+
+    /**
+     *  Helper method checks that checks if the attribute exists in the incoming instance. Checks for either
+     *  created or species values. Returns displayName or, if attribute instance doesn't exist,  null.
+     * @param instance GKInstance -- Instance being checked for an attribute.
+     * @param attribute String -- Attribute value that will be checked in the incoming instance.
+     * @return String -- Either the attribute instance's displayName, or null.
+     * @throws Exception -- Thrown by MySQLAdaptor.
+     */
+    public static String getInstanceAttributeNameForOutputReport(GKInstance instance, String attribute) throws Exception {
+        GKInstance attributeInstance = (GKInstance) instance.getAttributeValue(attribute);
+        return attributeInstance != null ? attributeInstance.getDisplayName() : "N/A";
     }
     
 }
