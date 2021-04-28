76002cc30296a0d0d46ed899f7b00e1bfec617d9
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2021-04-28 09:48:08 -0400

    Use ReactomeJavaConstants for resourceIdentifier.

--- src/main/java/org/reactome/release/qa/check/ReferenceDatabaseAccessURLCheck.java
+++ src/main/java/org/reactome/release/qa/check/ReferenceDatabaseAccessURLCheck.java
@@ -41,7 +41,7 @@ public class ReferenceDatabaseAccessURLCheck extends AbstractQACheck
 	private static final Logger logger = Logger.getLogger(ReferenceDatabaseAccessURLCheck.class);
 	private static final String ACCESS_URL_TOKEN = "urlPattern";
 	private static final String RESOURCE_IDENTIFIER_ENDPOINT = "https://registry.api.identifiers.org/restApi/resources/search/findByMirId?mirId=";
-	
+
 	/**
 	 * Queries identifiers.org with a resource identifier ("MIR:########").
 	 * (Copied from AddLinks)
@@ -51,9 +51,9 @@ public class ReferenceDatabaseAccessURLCheck extends AbstractQACheck
 	private String getAccessUrlForResource(String resourceIdentifier)
 	{
 		String accessUrl = null;
-		
+
 		String url = RESOURCE_IDENTIFIER_ENDPOINT + resourceIdentifier;
-		
+
 		try
 		{
 			HttpGet get = new HttpGet(new URI(url));
@@ -71,7 +71,7 @@ public class ReferenceDatabaseAccessURLCheck extends AbstractQACheck
 						accessUrl = responseObject.getString(ACCESS_URL_TOKEN).toString().replaceAll("\"", "");
 						break;
 					case HttpStatus.SC_NOT_FOUND:
-						// For a 404, we want to tell the user specifically what happened. 
+						// For a 404, we want to tell the user specifically what happened.
 						// This is the error code for invalid identifier strings.
 						// It should also be accompanied by the message: "Required {prefix}:{identifier}"
 						logger.error("Got 404 from identifiers.org for the resource identifier request: \"" + url + "\". You might want to verify that the resource identifier you requested is correct.");
@@ -92,28 +92,27 @@ public class ReferenceDatabaseAccessURLCheck extends AbstractQACheck
 			logger.error(e);
 			e.printStackTrace();
 		}
-		
+
 		return accessUrl;
 	}
-	
+
 	@Override
 	public QAReport executeQACheck() throws Exception
 	{
 		QAReport report = new QAReport();
 		report.setColumnHeaders("DB_ID", "diplayName", "oldAccessURL", "newAccessURL");
-		
+
 		@SuppressWarnings("unchecked")
 		Collection<GKInstance> refDatabases = dba.fetchInstancesByClass(ReactomeJavaConstants.ReferenceDatabase);
-		
+
 		for (GKInstance refDB : refDatabases)
 		{
-//			String resourceIdentifier = (String) refDB.getAttributeValue(ReactomeJavaConstants.resourceIdentifier);
-			String resourceIdentifier = (String) refDB.getAttributeValue("resourceIdentifier");
+			String resourceIdentifier = (String) refDB.getAttributeValue(ReactomeJavaConstants.resourceIdentifier);
 			if (resourceIdentifier != null && !resourceIdentifier.trim().isEmpty() )
 			{
 				String accessURLFromDB = (String) refDB.getAttributeValue(ReactomeJavaConstants.accessUrl);
 				String accessURLFromIdentifiersDotOrg = getAccessUrlForResource(resourceIdentifier);
-				
+
 				if (accessURLFromIdentifiersDotOrg != null && !accessURLFromIdentifiersDotOrg.trim().isEmpty())
 				{
 					accessURLFromIdentifiersDotOrg = accessURLFromIdentifiersDotOrg.replace(IDENTIFIERS_DOT_ORG_IDENTIFIER_TOKEN, REACTOME_IDENTIFIER_TOKEN);
